9549e92d5e387d45c4058e97847bbdcb1c8b49b2
Wallace Ferreira <mr.wallaceferreira@gmail.com>
2021-07-12 22:23:15 -0300

    Implementacao do carrosel

---
+++ js/owl/setup.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    stagePadding: 50,
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1200:{
+            items:3
+        },
+        2000:{
+            items:4
+        }
+    }
+})
